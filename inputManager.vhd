6505444ed3b0f85840dd6ae023936667e51afbef
tvanl <81866833+tvanl@users.noreply.github.com>
2021-04-10 17:28:33 -0400

    Update inputManager.vhd

--- inputManager.vhd
+++ inputManager.vhd
@@ -38,56 +38,28 @@ entity inputManager is
            addrB : out STD_LOGIC_VECTOR (9 downto 0);
            addrC : out STD_LOGIC_VECTOR (9 downto 0);     
            addrD : out STD_LOGIC_VECTOR (9 downto 0);
-           
+           count : in STD_LOGIC_VECTOR (9 downto 0);
            --numsOut: out vector_8bit(8 downto 0);
            
-           clr, clk: in STD_LOGIC
+           clr: in STD_LOGIC
            );
 end inputManager;
 
 architecture Behavioral of inputManager is
 
 
-signal timerA,timerB, timerC, timerD, count: STD_LOGIC_VECTOR(9 downto 0);--:=(others => '0');
+signal timerA,timerB, timerC, timerD: STD_LOGIC_VECTOR(9 downto 0):=(others => '0');
 signal aA, aB, aC, aD: STD_LOGIC_VECTOR(9 downto 0):=(others => '0');
 
 begin
 
-process(clk,clr)
+
+
+process(count)
 begin
-if clr = '1' then
-    count<="0000000000";
-
-    timerA<="0000000000";
-    timerB<="0000000000";
-    timerC<="0000000000";
-    
-    aA<="0000000000";
-    aB<="0000000000";
-    aC<="0000000000";
-elsif clk'event and clk = '1' then
-count<=count +1;
-end if;
-end process;
-
-process(clk,clr)
-variable newCount: STD_LOGIC_VECTOR (9 downto 0) := "0000000000";
-begin
-if clr = '1' then
-    count<="0000000000";
-
-    timerA<="0000000000";
-    timerB<="0000000000";
-    timerC<="0000000000";
-    
-    aA<="0000000000";
-    aB<="0000000000";
-    aC<="0000000000";
-elsif clk'event and clk = '1' then
-newCount:=Count+1;
-    timerA<=timerA+1;
-    timerB<=timerB+1;
-    timerC<=timerC+1;
+timerA<=timerA+1;
+timerB<=timerB+1;
+timerC<=timerC+1;
     if (timerA<9) then
         aA <= aA+1;
        
@@ -103,16 +75,56 @@ newCount:=Count+1;
         aA<=aA+3;
         timerA<="0000000001";
     end if;
-    if (newCount=3) then
+    
+    
+    
+        if (timerB<9) then
+        aB <= aB+1;
+       
+    elsif (timerB=9) then
+        aB<=aB+1;
+        aD<=aB+3;
+        
+    elsif (timerB=10) then
+        aB<=aB+1;
+        aD<=aB+3;
+        
+    elsif (timerB=11) then
+        aB<=aB+3;
+        timerB<="0000000001";
+    end if;
+   
+   
+   
+       if (timerC<9) then
+        aA <= aA+1;
+       
+    elsif (timerC=9) then
+        aC<=aC+1;
+        aD<=aC+3;
+        
+    elsif (timerC=10) then
+        aC<=aC+1;
+        aD<=aC+3;
+        
+    elsif (timerC=11) then
+        aC<=aC+3;
+        timerC<="0000000001";
+    end if;
+    
+    
+    if (Count=0) then
+        timerA<="0000000000";
+        aA<="0000000000";
+    end if;
+    if (Count=3) then
         timerB<="0000000000";
         aB<="0000001100";
     end if;
-    if (newCount=6) then
+    if (Count=6) then
         timerC<="0000000000";
         aC<="0000011000";
-    end if; 
-        count<=newCount;
-end if;   
+    end if;    
 end process;
 
 
