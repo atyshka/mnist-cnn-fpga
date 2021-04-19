a627c8cef47e12748e4d718bef9acfa03e5fee82
tvanl <81866833+tvanl@users.noreply.github.com>
2021-04-18 23:38:43 -0400

    Update full_sim.vhd

--- full_sim.vhd
+++ full_sim.vhd
@@ -23,17 +23,23 @@ component systolic_topfile is
     port(
         mclk: in std_logic;
         clr: in std_logic;
-        ld: out std_logic_vector(11 downto 0)
+        rgb: out std_logic_vector(11 downto 0);
+        an: out std_logic_vector(7 downto 0);
+        a_to_g: out std_logic_vector(6 downto 0);
+        hsync_out, vsync_out,dp: out std_logic;
+        ld: out std_logic_vector(3 downto 0)
     );
 end component;
  
-signal mclk, clr : STD_LOGIC := '0';               
+signal mclk, clr,hsync_out,vsync_out : STD_LOGIC := '0'; 
+signal rgb: std_logic_vector(11 downto 0); 
+signal ld: std_logic_vector(3 downto 0);              
 --signal num_in, num_out : vector_8bit(11 downto 0) := (others => x"00");
 
 constant T: time:= 10 ns;
 constant DC: real:= 0.5;
 begin
-    uut: systolic_topfile port map(mclk => mclk, clr => clr);
+    uut: systolic_topfile port map(mclk => mclk, clr => clr, rgb=>rgb,hsync_out=>hsync_out, vsync_out=>vsync_out,ld=>ld);
     
     clock_process: process
     begin
