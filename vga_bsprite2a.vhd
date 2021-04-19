d15b17a51f0a5b7b7068c5a59b7a93efce5e1180
tvanl <81866833+tvanl@users.noreply.github.com>
2021-04-19 19:13:03 -0400

    For Multiple Images

--- vga_bsprite2a.vhd
+++ vga_bsprite2a.vhd
@@ -7,7 +7,8 @@ entity vga_bsprite2a is
            hc : in std_logic_vector(9 downto 0);
            vc : in std_logic_vector(9 downto 0);
            dataROM: in std_logic_vector(7 downto 0);
-           rom_addr16: out std_logic_vector(9 downto 0);
+           rom_addr16: out std_logic_vector(12 downto 0);
+          counter : in std_logic_vector(3 downto 0);
            red, green, blue : out std_logic_vector(3 downto 0)
 	);
 end vga_bsprite2a;
@@ -28,17 +29,20 @@ begin
 	R1 <= '0' & "0000" & "00001";
 	ypix <= vc - vbp - R1;
 	xpix <= hc - hbp - C1;
-
+	
 	--Enable sprite video out when within the sprite region
  	spriteon <= '1' when (((hc > C1 + hbp) and (hc <= C1 + hbp + w))
            and ((vc >= R1 + vbp) and (vc < R1 + vbp + h))) else '0'; 
 	process(xpix, ypix)
 	variable  rom_addr1, rom_addr2: STD_LOGIC_VECTOR (9 downto 0);
+	variable  rom_addr3: STD_LOGIC_VECTOR (12 downto 0);
 	begin 
-		rom_addr1 := ('0' & ypix(6 downto 2) & "0000") + ("00" & ypix(6 downto 2) & "000") + ("000" & ypix(6 downto 2) & "00");	
+		rom_addr1 := ('0' & ypix(4 downto 0) & "0000") + ("00" & ypix(4 downto 0) & "000") + ("000" & ypix(4 downto 0) & "00");	
            -- y*(16+8+4) = y*28
-		rom_addr2 := rom_addr1 + ("00000" & xpix(6 downto 2)); -- y*28+x
-		rom_addr16 <= rom_addr2(9 downto 0);
+		rom_addr2 := rom_addr1 + ("00000" & xpix(4 downto 0)); -- y*28+x
+		rom_addr3 := ("0000"&rom_addr2) + ("00000" & counter & "0000") + ('0' & counter & "00000000") + (counter & "000000000");
+		-- y*() = y*28
+		rom_addr16 <= rom_addr3;
 	end process;
 	process(spriteon, vidon, dataROM, done)
  	begin
@@ -51,5 +55,5 @@ begin
     			blue <= (not dataROM(7)) & dataROM(6) & dataROM(5) & dataROM(4);
 		end if;
   	end process; 
-
+					
    end vga_bsprite2a;
